28535571c846d2bd8b67f45020cc49c244ff4b06
Michel <kintelalpz@gmail.com>
2021-11-13 21:34:21 +0100

    implémentation du routage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './plateforme/pages/accueil/accueil.component';
+import { AboutComponent } from './plateforme/pages/about/about.component';
+import { AideComponent } from './plateforme/pages/aide/aide.component';
+import { ErrorComponent } from './plateforme/pages/error/error.component';
+import { LoginComponent } from './plateforme/pages/login/login.component';
 
 const routes: Routes = [
 
@@ -10,14 +14,53 @@ const routes: Routes = [
     component: AccueilComponent,
     pathMatch:'full'
 
-  }
+  },
+
+  {
+    path: 'presentation',
+    component: AboutComponent,
+  
+  },
+
+  {
+    path: 'aide',
+    component: AideComponent,
+
+  },
+
+  {
+    path: 'connexion',
+    component: LoginComponent,
+
+  },
+
+  {
+    path: '**',
+    redirectTo: 'error',
+
+  },
+
+  {
+    path: 'error',
+    component: ErrorComponent,
+
+  },
+
+
+
 ];
 
 
 @NgModule({
   declarations: [],
+
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+
+  exports:[
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -20,6 +21,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ],
 
   imports: [
+    RouterModule,
     CommonModule
   ]
 })
